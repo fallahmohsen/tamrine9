c4ae29a5b7ff6cfc7123912f97357b83922519cc
mohsenfallah <mohsefalllah@gmail.com>
2022-07-05 17:39:46 +0430

    fix Average try catch

--- src/lamda/Average.java
+++ src/lamda/Average.java
@@ -1,17 +1,24 @@
 package lamda;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.function.BiFunction;
 
 public class Average {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("plz enter your first number:");
-        double number1= scanner.nextDouble();
-        System.out.println("plz enter your second number:");
-        double number2= scanner.nextDouble();
-        BiFunction<Double,Double,Double> average= (a, b) -> (a+b)/2;
-        System.out.format("The average of two numbers %.2f and %.2f is equal to %.2f.",
-                number1,number2,average.apply(number1,number2));
+        while (true) {
+            try {
+                Scanner scanner=new Scanner(System.in);
+                System.out.println("plz enter your first number:");
+                double number1 = scanner.nextDouble();
+                System.out.println("plz enter your second number:");
+                double number2 = scanner.nextDouble();
+                BiFunction<Double, Double, Double> average = (a, b) -> (a + b) / 2;
+                System.out.println("average is :" + average.apply(number1, number2));
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("wrong number");
+            }
+        }
     }
 }
